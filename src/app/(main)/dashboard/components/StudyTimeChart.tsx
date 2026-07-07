6a20e16f284870141dca53aa487d3b75b511f368
hafeez-07 <hafeezmohammad689@gmail.com>
2026-07-07 15:06:05 +0530

    feat(ui): Integrate Recharts for StudyTimeChart visualization

--- src/app/(main)/dashboard/components/StudyTimeChart.tsx
+++ src/app/(main)/dashboard/components/StudyTimeChart.tsx
@@ -1,19 +1,49 @@
+"use client";
+
+import {
+  ResponsiveContainer,
+  BarChart,
+  XAxis,
+  YAxis,
+  Bar,
+  Tooltip,
+  CartesianGrid,
+} from "recharts";
+
 export default function StudyTimeChart() {
-  const studyTime = [120, 80, 170, 30, 240, 60, 200];
-  const hours = ["16h", "12h", "8h", "4h", "0h"];
-  const last7Days = [
-    "Jul 1",
-    "Jul 2",
-    "Jul 3",
-    "Jul 4",
-    "Jul 5",
-    "Jul 6",
-    "Jul 7",
+  const lastWeekData = [
+    {
+      day: "Mon",
+      study: "1",
+    },
+    {
+      day: "Tue",
+      study: "3",
+    },
+    {
+      day: "Wed",
+      study: "5",
+    },
+    {
+      day: "Thur",
+      study: "7",
+    },
+    {
+      day: "Mon",
+      study: "2",
+    },
+    {
+      day: "Fri",
+      study: "4",
+    },
+    {
+      day: "Sat",
+      study: "6",
+    },
   ];
-  
   return (
-    <div className="p-6 mt-6 rounded bg-zinc-900 ">
-      <div className="flex justify-between">
+    <div className="p-6 mt-6 rounded bg-zinc-900 w-[50%]">
+      <div className="flex  justify-between">
         <h2 className="text-xl">
           <span className=" font-semibold">Study Time </span>( Last 7 days )
         </h2>
@@ -21,32 +51,41 @@ export default function StudyTimeChart() {
           <span className="font-bold">Total</span> : 28h 40m
         </p>
       </div>
-      <div className="flex">
-        <div className="mt-6 space-y-4">
-          {hours.map((hour, index) => (
-            <div key={index} className="pb-6">
-              {hour}
-            </div>
-          ))}
-        </div>
-        <div className="ml-10 flex gap-7 items-end">
-          {studyTime.map((time, index) => (
-            <div
-              key={index}
-              style={{
-                height: `${time}px`,
-              }}
-              className="w-10 bg-emerald-400"
-            ></div>
-          ))}
-        </div>
-      </div>
-
-      <div className="ml-10 flex gap-7">
-        {last7Days.map((day, index) => (
-          <div key={index}>{day}</div>
-        ))}
-      </div>
+      <ResponsiveContainer width="100%" height={200} className=" mt-6">
+        <BarChart data={lastWeekData} margin={{ left: -20, top: 10 }}>
+          <XAxis
+            dataKey="day"
+            tickLine={false}
+            tick={{
+              fontSize: 12,
+              fill: "gray",
+            }}
+          />
+          <YAxis
+            tickLine={false}
+            tickFormatter={(value) => `${value}h`}
+            tick={{
+              fontSize: 12,
+              fill: "gray",
+            }}
+          />
+          <Bar dataKey="study" fill="#00BC7D" />
+          <CartesianGrid vertical={false} stroke="gray" strokeDasharray="4 4" />
+          <Tooltip
+            cursor={false}
+            contentStyle={{
+              backgroundColor: "black",
+              border: "none",
+              borderRadius: "5%",
+              fontSize: 12,
+              color:"white"
+            }}
+            itemStyle={{
+              color: "white",
+            }}
+          />
+        </BarChart>
+      </ResponsiveContainer>
     </div>
   );
 }
